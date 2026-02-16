a905776ebf32bc19c3e983952dffa8dc46877903
Arthur Pereira da Silva <72280602+ArthurPdaSilva@users.noreply.github.com>
2026-02-16 16:51:07 -0300

    feat db

---
+++ src/db/seeds.py
@@ -0,0 +1,22 @@
+USUARIOS = [
+    (1, "Arthur", 5537.70),
+    (2, "Yasmin", 1800.00),
+]
+
+CONTAS = [
+    (1, 1, "Internet", 80.00),
+    (2, 1, "Energia", 30.00),
+    (3, 1, "Água", 80.00),
+    (4, 1, "Streaming", 44.00),
+    (5, 1, "Bloqueador", 10.00),
+    (6, 1, "Spotify-Arthur", 12.90),
+    (7, 2, "Spotify-Yasmin", 12.90),
+    (8, 1, "Academia", 90.00),
+    (9, 1, "Feira", 340.00),
+]
+
+DIVIDAS = [
+    (2, 2, "Geladeira", 1049.93, 6),
+    (3, 1, "Cadeira", 683.14, 1),
+    (4, 1, "Cerâmica", 2199.96, 4),
+]
